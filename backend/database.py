e20d96f2cad234637574c3e4e936decbfb9ee4cd
Bhaumik senwal <bhaumiksenwal99@gmail.com>
2026-03-24 10:56:54 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -8,7 +8,7 @@ engine = create_engine(
     settings.DATABASE_URL,
     connect_args={"check_same_thread": False} if "sqlite" in settings.DATABASE_URL else {},
     echo=settings.DEBUG
-)
+) 
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
