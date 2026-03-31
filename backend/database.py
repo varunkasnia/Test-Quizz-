9e85f2a39782c911789940d8353ec0e075b6e4ed
Bhaumik senwal <2301730328@krmu.edu.in>
2026-03-31 23:31:08 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, Column, Integer, String, Text, DateTime, F
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 from datetime import datetime
-from config import settings    
+from config import settings
   
 engine = create_engine( 
     settings.DATABASE_URL,
