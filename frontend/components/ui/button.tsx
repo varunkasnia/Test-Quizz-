bb3ffb711e535264f774f867e8bd9bd4019d4f12
Bhaumik senwal <2301730328@krmu.edu.in>
2026-03-31 23:33:34 +0530

    Update button.tsx

--- frontend/components/ui/button.tsx
+++ frontend/components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default:
+        default: 
           "bg-primary text-primary-foreground shadow hover:bg-primary/90",
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
