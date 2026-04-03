1b0f3b4cce9c50a7ac988ff02c069e58965c142d
Bhaumik senwal <bhaumiksenwal99@gmail.com>
2026-04-04 01:12:13 +0530

    Update socket.ts

--- frontend/lib/socket.ts
+++ frontend/lib/socket.ts
@@ -1,7 +1,7 @@
 import { io, Socket } from 'socket.io-client';
 
 // 1. Ensure this ENV variable in Render is: https://certifiacate-app-1.onrender.com
-const WS_URL = process.env.NEXT_PUBLIC_WS_URL || 'http://localhost:8000';
+const WS_URL = process.env.NEXT_PUBLIC_WS_URL || 'http://localhost:8000'; 
  
 let socket: Socket | null = null;
 
