9a76c0d0cc9d11f1353aff13fc5aaa2058834878
varunkasnia <varunkasnia73@gmail.com>
2026-04-07 15:20:31 +0530

    Update next-env.d.ts

--- frontend/next-env.d.ts
+++ frontend/next-env.d.ts
@@ -1,5 +1,5 @@
-   /// <reference types="next" />
-/// <reference types="next/image-types/global" />
+   / <reference types="next" />
+/ <reference types="next/image-types/global" />
          
 // NOTE: This file should not be edite d  
 // see https://nextjs.org/docs/app/building-your-application/configuring/typescript for more information. 
