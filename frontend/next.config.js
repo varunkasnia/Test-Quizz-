1655da96f59325be8d977618c4de0ca300f132f8
Bhaumik senwal <bhaumiksenwal99@gmail.com>
2026-04-14 23:59:48 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_API_URL:  process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000',
     NEXT_PUBLIC_WS_URL:  process.env.NEXT_PUBLIC_WS_URL || 'http://localhost:8000', 
-  },
+  }, 
 }
 
 module.exports = nextConfig 
